fb02a6fee1250dfebb8afa667b6e6241a02d4780
Stav42 <vaditya@iitk.ac.in>
2023-10-20 10:41:07 +0530

    ppo implemented and working fine

--- cartpole/ppo/ppo_parallel.py
+++ cartpole/ppo/ppo_parallel.py
@@ -19,7 +19,6 @@ from torch.utils.tensorboard import SummaryWriter
 from debugging import check_values_same
 from helper import parse_args
 import functools
-from multiprocessing import Pool, Manager
 
 torch.set_printoptions(threshold=10000)
 
@@ -115,8 +114,8 @@ class Simulation:
         self.value_buffer = torch.zeros((args.num_steps, args.num_envs))
         self.td_buffer = torch.zeros((args.num_steps, args.num_envs))
         self.gae_buffer = torch.zeros((args.num_steps, args.num_envs))
-        self.obs_buffer = torch.zeros((args.num_steps, args.num_envs))
-        self.action_buffer = torch.zeros((args.num_steps, args.num_envs))
+        self.obs_buffer = torch.zeros((args.num_steps, args.num_envs, self.obs_space_dim))
+        self.action_buffer = torch.zeros((args.num_steps, args.num_envs, self.action_space_dim))
         self.epsilon = 0
         self.wandb_run = None
         self.global_eps = 0
@@ -343,10 +342,11 @@ class Simulation:
         clipfracs = []
         for epoch in range(args.update_epochs):
             np.random.shuffle(b_inds)
-            b_obs = self.obs_buffer.reshape(-1)
-            b_actions = self.action_buffer.reshape(-1)
+            b_obs = self.obs_buffer.reshape((-1,) + self.envs.observation_space.shape)
+            b_actions = self.action_buffer.reshape((-1,) + self.envs.action_space.shape)
             b_logprob = self.log_prob_buffer.reshape(-1)
             b_gae = self.gae_buffer.reshape(-1)
+            b_val = self.value_buffer.reshape(-1)
             for start in range(0, args.batch_size, args.minibatch_size):
                 end = start + args.minibatch_size
                 mb_inds = b_inds[start:end]
@@ -356,12 +356,11 @@ class Simulation:
                 logratio = new_logprob - b_logprob[mb_inds]
                 ratio = logratio.exp()
                 mb_advantages = b_gae[mb_inds]
-
-                pg_loss1 = -mb_advantages*ratio
-                pg_loss2 = -mb_advantages*torch.clamp(ratio, 1 - args.clip_coef, 1 + args.clip_coef)
+                pg_loss1 = -mb_advantages.detach()*ratio
+                pg_loss2 = -mb_advantages.detach()*torch.clamp(ratio, 1 - args.clip_coef, 1 + args.clip_coef)
                 pg_loss = torch.max(pg_loss1, pg_loss2).mean()
 
-                value_loss = ((new_val - self.value_buffer[mb_inds])**2).mean()
+                value_loss = ((new_val - b_val[mb_inds])**2).mean()
             
                 self.pol_optimizer.zero_grad()
                 pg_loss.backward()
@@ -382,6 +381,7 @@ class Simulation:
         obs = self.envs.reset()
 
         for update in range(1, num_upd+1):
+            print(f"Update: {update}")
             obs = self.envs.reset()
             obs_tensor = torch.tensor(np.array(obs), dtype=torch.float32)
             step_time = 0
@@ -394,14 +394,16 @@ class Simulation:
             for step in range(0, args.num_steps):
                 global_step+=1*args.num_envs
                 self.steps+=1
-                action, log_probability = self.sample_action(obs)
+                with torch.no_grad():
+                    action, log_probability = self.sample_action(obs)
                 self.log_prob_buffer[step, :] = log_probability
                 obs_tensor = torch.tensor(np.array(obs), dtype=torch.float32)
+                val = self.value.forward(obs_tensor)
+                with torch.no_grad():
+                    self.value_buffer[step, :] = torch.transpose(val, 0, 1)
+                obs, reward, done, info = self.envs.step(action)
                 self.obs_buffer[step, :] = obs_tensor
                 self.action_buffer[step, :] = action
-                val = self.value.forward(obs_tensor)
-                self.value_buffer[step, :] = torch.transpose(val, 0, 1)
-                obs, reward, done, info = self.envs.step(action)
                 for i in range(len(env_steps)):
                     env_steps[i] += 1
                 self.reward_buffer[step, :] = torch.tensor(reward)
@@ -416,14 +418,15 @@ class Simulation:
                 
             step_time/=global_step
             self.eps_run+=1
-            self.get_return_buffer(masks)
-            self.get_td_buffer(masks)
-            self.get_gae_buffer(lmbda=0.99, masks=masks)
+            with torch.no_grad():
+                self.get_return_buffer(masks)
+                self.get_td_buffer(masks)
+                self.get_gae_buffer(lmbda=0.99, masks=masks)
             self.learn()
             self.log_data()
             self.flush_post_ep()
 
-            if (update) % 40 == 0:
+            if (update) % 10 == 0:
                 avg_reward = self.log_avg_reward[-1]
                 print("update:", update, "Average Reward:", avg_reward, "Average Return: ", self.log_avg_return[-1], "Episode Length: ", np.array(self.episode_length[-10:]).mean())
 
